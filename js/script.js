ca34d5a5d58a23c6432e471031561780b34c64d2
mnap00 <learn@10g.pl>
2018-03-08 17:57:45 +0100

    Add jQuery scripts

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+    console.log('DOM loaded');
+});
+
+var span = $('span');
+span.each(function(index, element) {
+    if(index % 2 == 0) {
+        $(element).css('color', 'red');
+    }
+});
+
+// or
+//$('span:even').css('color', 'red');
+
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+    var button = '<button class="btn" data-tmp="' + index + '">Click me!</button>';
+    $(element).append(button);
+});
+
+$('button').click(function() {
+    alert($(this).attr("data-tmp"));
+});
